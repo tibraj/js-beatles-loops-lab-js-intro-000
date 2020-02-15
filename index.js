d7e0bb1188cf3b33d44a316cab11d447d5652c65
Taulant Ibraj <tibraj94@gmail.com>
2020-02-15 02:38:17 +0000

    Done.

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ function theBeatlesPlay(players, instruments) {
   return members;
 }
 
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+function johnLennonFacts(facts) {
   var exclaimedFacts = [];
   var i = 0;
   while (i < facts.length) {
     exclaimedFacts.push(facts[i] + "!!!");
+    i++;
   }
   return exclaimedFacts;
 }
@@ -25,6 +22,6 @@ function iLoveTheBeatles (num) {
   do{
     praise.push("I love the Beatles!");
     num++;
-  } while(num < 15);
+  } while (num < 15);
   return praise;
 }
