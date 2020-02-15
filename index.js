c2d5d8b5c91e096fb094040ad06282ba88158bd0
Taulant Ibraj <tibraj94@gmail.com>
2020-02-15 02:37:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(players, instruments) {
+  var members = [];
+  for (var i = 0; i < players.length; i++) {
+    members.push(players[i] + " plays " + instruments[i]);
+  }
+  return members;
+}
+
+function johnLennonFacts(facts) {
+  var exclaimedFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    exclaimedFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return exclaimedFacts;
+}
+
+function iLoveTheBeatles (num) {
+  var praise = [];
+  do{
+    praise.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  return praise;
+}
